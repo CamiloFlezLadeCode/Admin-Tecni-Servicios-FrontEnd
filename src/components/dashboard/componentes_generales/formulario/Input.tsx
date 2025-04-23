dc939250c496dbd0231d9c4578471c4d6096513a
CamiloFlezLadeCode <camilodesarrollador@outlook.com>
2025-04-23 00:25:08 -0500

    Se inicia creación de componentes_generales para formularios dentro de dashboard

---
+++ src/components/dashboard/componentes_generales/formulario/Input.tsx
@@ -0,0 +1,48 @@
+// components/InputText.tsx
+import React from 'react';
+import { FormControl, InputLabel, TextField, Grid, OutlinedInput } from '@mui/material';
+
+
+interface InputTextProps {
+    label: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    required?: boolean;
+    tamano: 'small' | 'medium'; // Especifica los tamaños permitidos
+    tipo_input: 'input' | 'date' | 'time' | 'number';
+}
+
+const InputText: React.FC<InputTextProps> = ({ label, value, onChange, required = false, tamano, tipo_input }) => {
+    const [focused, setFocused] = React.useState(false);
+    return (
+        <FormControl fullWidth required={required} variant="outlined">
+            <InputLabel htmlFor={label} shrink
+                // style={{color: '#000000', fontWeight: 'bolder'}}
+                // style={{
+                //     color: value ? '#000000' : '#B0B0B0', // Color gris si no hay valor
+                //     fontWeight: 'bolder',
+                // }}
+                style={{
+                    // color: focused || value ? '#000000' : '#B0B0B0', // Color negro si está enfocado o tiene valor
+                    color: focused ? '#000000' : 'gray', // Color negro si está enfocado o tiene valor
+                    fontWeight: 'bolder',
+                }}
+            >
+                {label}
+            </InputLabel>
+            <OutlinedInput
+                id={label} // Vinculación correcta
+                value={value}
+                onChange={onChange}
+                label={label} // Asegúrate de incluir el label aquí
+                size={tamano}
+                type={tipo_input}
+                notched // Esta propiedad hace que la muesca sea visible siempre
+                onFocus={() => setFocused(true)} // Cambiar estado a enfocado
+                onBlur={() => setFocused(false)} // Cambiar estado a no enfocado
+            />
+        </FormControl>
+    );
+};
+
+export default InputText;
---
+++ src/components/dashboard/componentes_generales/formulario/Select.tsx
@@ -0,0 +1,37 @@
+// components/InputSelect.tsx
+import React from 'react';
+import { FormControl, InputLabel, Select, MenuItem, Grid, SelectChangeEvent } from '@mui/material';
+
+interface InputSelectProps {
+  label: string;
+  value: string;
+  onChange: (event: SelectChangeEvent<string>) => void; // Cambiado a SelectChangeEvent
+  options: { value: string; label: string }[]; // Array de opciones
+  required?: boolean;
+  size?: 'small' | 'medium'; // Tamaño del select
+}
+
+const InputSelect: React.FC<InputSelectProps> = ({ label, value, onChange, options, required = false, size = 'small' }) => {
+  return (
+    <Grid item md={3} xs={12} mt={1}>
+      <FormControl fullWidth required={required}>
+        <InputLabel>{label}</InputLabel>
+        <Select
+          value={value}
+          onChange={onChange}
+          label={label}
+          variant="outlined"
+          size={size}
+        >
+          {options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Grid>
+  );
+};
+
+export default InputSelect;
